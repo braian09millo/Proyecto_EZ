91d49f05ea71540424a9be0cfdc51c06c172cadb
tomasromero88 <tomasromero88@gmail.com>
2019-05-05 18:52:42 -0300

    Agrego Rpt de Facturación para informes de totales por cliente

---
+++ Proyecto_EZ/Stored Procedures/spRptFacturacion.sql
@@ -0,0 +1,29 @@
+IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('spRptFacturacion') and sysstat & 0xf = 4)
+	DROP PROCEDURE spRptFacturacion
+GO
+
+CREATE PROCEDURE spRptFacturacion		(@FechaDesde datetime, @FechaHasta datetime,@Repartidor int)												
+WITH ENCRYPTION AS
+
+	DECLARE @@nRet INT
+
+SELECT max(cli_nombre) as Nombre,
+		count(*) as [Cantidad Pedidos],
+		SUM(ped_monto) [Monto Remitos],
+		SUM(ped_resto) [Debe],
+		SUM(ped_monto)-SUM(ped_resto) Facturado,
+		max(usu_nombre) Repartidor
+	FROM Pedido
+	JOIN cliente on cli_id = ped_cliente
+	LEFT JOIN usuario on ped_repartidor = usu_usuario
+	WHERE ped_fecha between @FechaDesde and @FechaHasta
+	and (ped_repartidor = @Repartidor or @repartidor is null)
+	group by cli_id,usu_usuario
+
+
+	SET @@nRet = @@error
+	IF @@nRet <> 0 
+		RETURN @@nRet
+
+RETURN 0
+GO
