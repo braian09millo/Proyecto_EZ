e56ff5f1dcf94e92f4511e17156ce30711c32304
braian09millo <braian09.millo@gmail.com>
2019-06-05 23:20:36 -0300

    Ganancia Mensual Bruta (Gráfico)

--- Proyecto_EZ/Stored Procedures/spDashboardDatosGrafico.sql
+++ Proyecto_EZ/Stored Procedures/spDashboardDatosGrafico.sql
@@ -17,22 +17,22 @@ WITH ENCRYPTION AS
 	group by DATENAME(MONTH, DATEADD(MM, s.number, CONVERT(DATETIME, 0))), MONTH(DATEADD(MM, s.number, CONVERT(DATETIME, 0)))
 	ORDER BY 2
 
-	SELECT DATENAME(MONTH, DATEADD(MM, s.number, CONVERT(DATETIME, 0))) AS Mes, 
-	MONTH(DATEADD(MM, s.number, CONVERT(DATETIME, 0))) AS MesNumero,
-	SUM(gas_monto) AS GastoMensual
-	INTO #GastoMensual 
-	FROM master.dbo.spt_values s 
-	LEFT JOIN gasto ON MONTH(gas_fecha) - 1  = s.number AND YEAR(gas_fecha) = YEAR(CURRENT_TIMESTAMP)
-	WHERE [type] = 'P' AND s.number BETWEEN 0 AND 11 
-	GROUP BY DATENAME(MONTH, DATEADD(MM, s.number, CONVERT(DATETIME, 0))), MONTH(DATEADD(MM, s.number, CONVERT(DATETIME, 0)))
-	ORDER BY 2
+	--SELECT DATENAME(MONTH, DATEADD(MM, s.number, CONVERT(DATETIME, 0))) AS Mes, 
+	--MONTH(DATEADD(MM, s.number, CONVERT(DATETIME, 0))) AS MesNumero,
+	--SUM(gas_monto) AS GastoMensual
+	--INTO #GastoMensual 
+	--FROM master.dbo.spt_values s 
+	--LEFT JOIN gasto ON MONTH(gas_fecha) - 1  = s.number AND YEAR(gas_fecha) = YEAR(CURRENT_TIMESTAMP)
+	--WHERE [type] = 'P' AND s.number BETWEEN 0 AND 11 
+	--GROUP BY DATENAME(MONTH, DATEADD(MM, s.number, CONVERT(DATETIME, 0))), MONTH(DATEADD(MM, s.number, CONVERT(DATETIME, 0)))
+	--ORDER BY 2
 
 	SELECT
 		G1.Mes,
 		G1.MesNumero,
-		ISNULL(G1.GananciaMensual,0) - ISNULL(G2.GastoMensual,0) AS GananciaTotal
+		ISNULL(G1.GananciaMensual,0) /*- ISNULL(G2.GastoMensual,0)*/ AS GananciaTotal
 	FROM #GananciaMensual G1
-	JOIN #GastoMensual G2 ON G1.MesNumero = G2.MesNumero
+	--JOIN #GastoMensual G2 ON G1.MesNumero = G2.MesNumero
 
 	SET @@nRet = @@error
 	IF @@nRet <> 0 
