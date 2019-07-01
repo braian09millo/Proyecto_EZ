438398aad32047fbdb16b50a5ac9ff8f254c0424
braian09millo <braian09.millo@gmail.com>
2019-06-30 22:57:02 -0300

    Facturación Mensual: - Encabezado con nombre de informe, filtros y logos

--- Proyecto_EZ/App/Controllers/ReporteController.cs
+++ Proyecto_EZ/App/Controllers/ReporteController.cs
@@ -42,7 +42,12 @@ namespace App.Controllers
             listas.Add(xoCtrlGasto.ObtenerGastos(xdFechaDesde, xdFechaHasta, out xsError));
             listas.Add(xoCtrlPedido.ObtenerFacturacionResultadoRpt(xdFechaDesde, xdFechaHasta, xsRepartidor));
 
-            var bytes = Reporting.GenerarInforme(listas, _path, _nombre, _nombresDs, "PDF", null);
+            var parFechaDesde = new ReportParameter("FechaDesde", xdFechaDesde.ToString("dd-MM-yy"));
+            var parFechaHasta = new ReportParameter("FechaHasta", xdFechaHasta.ToString("dd-MM-yy"));
+            var parRepartidor = new ReportParameter("Repartidor", xsRepartidor);
+            var listaParametros = new List<ReportParameter>() { parFechaDesde, parFechaHasta, parRepartidor };
+
+            var bytes = Reporting.GenerarInforme(listas, _path, _nombre, _nombresDs, "PDF", listaParametros);
             return File(bytes, "application/pdf", "FacturacionMensual_" + xdFechaDesde.ToString("ddMMyyyy") + "_" + xdFechaHasta.ToString("ddMMyyyy") + ".pdf");
         }
 
