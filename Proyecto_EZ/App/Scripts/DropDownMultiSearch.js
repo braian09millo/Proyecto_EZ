d80b0085e4986affa5b42c2e58bf6ff45ec1897d
TELECTRONICA\bmoretti <bmoretti@telectronicapeaje.com>
2019-07-23 08:38:11 -0300

    Dropdownlist con busqueda y selección

---
+++ Proyecto_EZ/App/Scripts/DropDownMultiSearch.js
@@ -0,0 +1,26 @@
+﻿$('.DropDownMultiSearch').multiselect({
+    includeSelectAllOption: true,
+    selectAllValue: "",
+    selectAllText: $('#Traduccion_Todos').val(),
+    nSelectedText: ' - ' + $('#Traduccion_OpcionesSeleccionadas').val(),
+    nonSelectedText: $('#Traduccion_SeleccioneUnaOpcion').val(),
+    enableFiltering: true,
+    enableCaseInsensitiveFiltering: true,
+    filterPlaceholder: $('#Traduccion_Buscar').val(),
+    numberDisplayed: 1,
+    allSelectedText: $('#Traduccion_Todos').val(),
+    dropUp: false
+});
+
+function reloadDropDownList(id, elements) {
+    var ddl = $(id)
+    ddl.empty();
+                        
+    for (var i = 0; i < elements.length; i++) {
+        ddl.append($('<option></option>').val(elements[i].Value).html(elements[i].Text));
+    }
+                        
+    ddl.multiselect('rebuild')
+    ddl.multiselect('selectAll',false)
+    ddl.multiselect('updateButtonText')
+}
