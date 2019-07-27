ead26afdedf090605ee32eafc5736541514ec49c
tomasromero88 <tomasromero88@gmail.com>
2019-07-26 23:08:52 -0300

    Reportes del dashboard

--- Proyecto_EZ/Entidades/spGetProductosMasVendidos.cs
+++ Proyecto_EZ/Entidades/spGetProductosMasVendidos.cs
@@ -11,5 +11,9 @@ namespace Entidades
         public int IdProducto { get; set; }
         public string Producto { get; set; }
         public int CantidadPacks { get; set; }
+        public long Ranking { get; set; }
+        public decimal Porcentaje { get; set; }
+        public int VentasUnitarias { get; set; }
+        public decimal TotalFacturado { get; set; }
     }
 }
